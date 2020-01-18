38ebee1b6474ab6a58b796aa580f126750603931
Renad-Ahmad <rena.34end@gmail.com>
2020-01-18 03:45:22 +0300

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+  {
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds.png"
+},
+];
 cardsInPlay = [];
 function checkForMatch(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -15,9 +36,12 @@ function flipCard(cardId){
     alert("Sorry, try again.");
   }
 };
-console.log("User flipped" +" " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped" +" " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage );
+console.log(cards[cardId].suit );
 };
 flipCard(0);
 flipCard(2);
 checkForMatch ();
+ alert("Sorry, try again.");
